48cae891b4edf0da0fb99667b69bd84fc660cee3
sorokaOleksandr <soroka.oleksandr333@gmail.com>
2019-07-23 23:13:52 +0300

    sending mail

--- PHP/Shop/config/web.php
+++ PHP/Shop/config/web.php
@@ -34,7 +34,15 @@ $config = [
             // send all mails to a file by default. You have to set
             // 'useFileTransport' to false and configure a transport
             // for the mailer to send real emails.
-            'useFileTransport' => true,
+            'useFileTransport' => false,
+           /* 'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => '',
+                'username' => '',
+                'password' => '',
+                'port' => '',
+                'encryption' => '',
+            ], */
         ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
